ea31b7a1cde5e213a012e0a7d6ad9557d63bd8c5
Maarten Broekman <maarten@broekman.us>
2024-01-06 18:54:11 -0500

    mjbroekman - added physics to tiles ; added game over for falling ; added movement and jumping

--- 2d-platformer/Scripts/Player.gd
+++ 2d-platformer/Scripts/Player.gd
@@ -1,5 +1,10 @@
 extends CharacterBody2D
 
+var move_speed : float = 100.0
+var jump_force : float = 200.0
+var gravity : float = 500.0
+var can_double_jump : bool = false
+var has_double_jump : bool = false
 #
 #const SPEED = 300.0
 #const JUMP_VELOCITY = -400.0
@@ -8,21 +13,31 @@ extends CharacterBody2D
 #var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 #
 #
-#func _physics_process(delta):
+func _physics_process(delta):
 	## Add the gravity.
-	#if not is_on_floor():
-		#velocity.y += gravity * delta
+	if not is_on_floor():
+		velocity.y += gravity * delta
 #
-	## Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
+	velocity.x = 0
+	if Input.is_key_pressed(KEY_LEFT):
+		velocity.x -= move_speed
+	
+	if Input.is_key_pressed(KEY_RIGHT):
+		velocity.x += move_speed
+
+	# Handle jumping
+	if Input.is_key_pressed(KEY_SPACE) and ( is_on_floor() or can_double_jump):
+		if not is_on_floor():
+			can_double_jump = false
+		velocity.y -= jump_force
 #
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction = Input.get_axis("ui_left", "ui_right")
-	#if direction:
-		#velocity.x = direction * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
+	if is_on_floor() and has_double_jump:
+		can_double_jump = true
+
+	if global_position.y > 150:
+		game_over()
 #
-	#move_and_slide()
+	move_and_slide()
+
+func game_over():
+	get_tree().reload_current_scene()
