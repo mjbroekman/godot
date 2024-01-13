6bc1d39b93bc0e97d016737baa9bf087cfff3564
Maarten Broekman <maarten@broekman.us>
2024-01-13 13:18:45 -0500

    mjbroekman - EnemyUnit scene and script with randomized sprite

---
+++ 2D-RTS/Scripts/EnemyUnit.gd
@@ -0,0 +1,8 @@
+extends Unit
+
+var rng = RandomNumberGenerator.new()
+
+func _ready():
+	super._ready()
+	var enemy_id = rng.randi_range(108,112)
+	sprite.texture = load(str("res://Sprites/tile_0",enemy_id,".png"))
