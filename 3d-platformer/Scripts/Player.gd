b69afe46e83966a0238c079178349c72932f2b6b
Maarten Broekman <maarten@broekman.us>
2024-02-11 21:01:50 -0500

    mjbroekman - added platforms ; jumping ; and falling

--- 3d-platformer/Scripts/Player.gd
+++ 3d-platformer/Scripts/Player.gd
@@ -9,11 +9,32 @@ var facing_angle : float
 @onready var model : MeshInstance3D = get_node("Model")
 
 func _physics_process(delta):
+	if not is_on_floor():
+		print(velocity.y)
+		velocity.y -= gravity * delta
+
+	if is_on_floor() and Input.is_action_pressed("jump"):
+		velocity.y += jump_force
+
+	if velocity.y < -30.0:
+		game_over()
+
 	# Create a Vector2 from our right/left/backward/forward movement
 	var input = Input.get_vector("move_right","move_left","move_backward","move_forward")
+
 	# Convert our Vector2 (left/right = .x, forward/back = .y) input into a useful Vector3 (left/right, up/down, forward/back)
 	var direction = Vector3(input.x,0,input.y)
 
+	# Set vectors individually
 	# velocity is defined in CharacterBody3D
-	velocity = direction * move_speed
+	velocity.x = direction.x * move_speed
+	velocity.z = direction.z * move_speed
+
 	move_and_slide()
+	
+	facing_angle = Vector2(input.y,input.x).angle()
+	if input.length() > 0:
+		model.rotation.y = lerp_angle(model.rotation.y,facing_angle,0.25)
+
+func game_over():
+	get_tree().reload_current_scene()
