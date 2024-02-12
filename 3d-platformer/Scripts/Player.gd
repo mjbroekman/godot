a0e8f90546323a399556ba0e51329c5410f502bf
Maarten Broekman <maarten@broekman.us>
2024-02-11 20:27:57 -0500

    mjbroekman - movement wasn't backward, arguments were

--- 3d-platformer/Scripts/Player.gd
+++ 3d-platformer/Scripts/Player.gd
@@ -9,7 +9,7 @@ var facing_angle : float
 @onready var model : MeshInstance3D = get_node("Model")
 
 func _physics_process(delta):
-	# Create a Vector2 from our left/right/forward/back movement
+	# Create a Vector2 from our right/left/backward/forward movement
 	var input = Input.get_vector("move_right","move_left","move_backward","move_forward")
 	# Convert our Vector2 (left/right = .x, forward/back = .y) input into a useful Vector3 (left/right, up/down, forward/back)
 	var direction = Vector3(input.x,0,input.y)
