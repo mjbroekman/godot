49ddbdb5eee672c852dae098214cb3dcd2c9dda6
Maarten Broekman <maarten@broekman.us>
2024-03-04 12:52:06 -0500

    mjbroekman - removed some spike traps ; finished end flag code

--- 3d-platformer/Scripts/EndFlag.gd
+++ 3d-platformer/Scripts/EndFlag.gd
@@ -7,7 +7,9 @@ var next_scene_file : String
 func _ready():
 	next_scene += 1
 	next_scene_file = str(scene_name,next_scene,".tscn")
+	print(str("Next Level is: ",next_scene_file))
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
-		pass
+		print(str("Switching to ",next_scene_file))
+		get_tree().change_scene_to_file(next_scene_file)
