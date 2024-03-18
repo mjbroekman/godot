ec8f2e0969fc7ea0d97d219aef233d0038a60ffc
Maarten Broekman <maarten@broekman.us>
2024-03-18 12:36:52 -0400

    mjbroekman - finish up script for toggling the inventory window, adding / removing items

--- inventory/Scripts/Inventory.gd
+++ inventory/Scripts/Inventory.gd
@@ -7,30 +7,75 @@ var inventory_slots : Array[InventorySlot]
 @export var starter_items : Array[Item]
 
 func _ready():
-	pass
+	toggle_window(false)
+	
+	for slot in get_node('InventoryPanel/InventoryGrid').get_children():
+		inventory_slots.append(slot)
+		slot.set_item(null)
+		slot.inventory = self
+
+	for item in starter_items:
+		add_item(item)
 
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("inventory"):
+		toggle_window(!window.visible)
 
 func toggle_window(open : bool):
-	pass
+	window.visible = open
+
+	if open:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	else:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func on_give_player_item(new_item : Item, amount : int):
-	pass
+	for i in range(amount):
+		add_item(new_item)
 
 func add_item(new_item : Item):
-	pass
+	var slot = get_slot_to_add(new_item)
+
+	if slot == null:
+		return
+
+	if slot.item == null:
+		slot.item = new_item
+	elif slot.item == new_item:
+		slot.add_item() # Call the add_item() function in the InventorySlot script
 
 func remove_item(old_item : Item):
-	pass
+	var slot = get_slot_to_remove(old_item)
+
+	if slot == null or slot.item == null:
+		return
+
+	slot.remove_item()
 
 func get_slot_to_add(new_item : Item) -> InventorySlot:
+	for slot in inventory_slots:
+		if slot.item == new_item and slot.quantity < new_item.max_stack_size:
+			return slot
+
+	for slot in inventory_slots:
+		if slot.item == null:
+			return slot
+
 	return null
 
 func get_slot_to_remove(old_item : Item) -> InventorySlot:
+	for slot in inventory_slots:
+		if slot.item == old_item:
+			return slot
+
 	return null
 
 func get_num_of_item(item : Item) -> int:
-	return 0
+	var total = 0
+	for slot in inventory_slots:
+		if slot.item == item:
+			total += slot.quantity
+
+	return total
 
 
