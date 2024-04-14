e39da5eeb9b3cd4238397bfe18b369134069d4f6
Maarten Broekman <maarten@broekman.us>
2024-04-14 13:28:44 -0400

    completed player movement control with acceleration / braking

--- walking-simulator/Scripts/Player.gd
+++ walking-simulator/Scripts/Player.gd
@@ -2,10 +2,10 @@ extends CharacterBody3D
 class_name PlayerController
 
 @export_group("Movement")
-@export var walk_speed : float = 4.0
-@export var run_speed : float = 8.0
-@export var acceleration : float = 20.0
-@export var braking_speed : float = 20.0
+@export var walk_speed : float = 5.0
+@export var run_speed : float = 15.0
+@export var acceleration : float = 15.0
+@export var braking_speed : float = 10.0
 @export var air_accel : float = 4.0
 @export var jump_force : float = 8.0
 @export var gravity_mod : float = 1.5 # this is a modifier on the base project gravity
@@ -40,11 +40,20 @@ func _physics_process(delta):
 		target_speed = run_speed
 		# get the dot product between our facing (transform) and move direction
 		var run_dot = -move_direction.dot(transform.basis.z)
+		# clamp to 0 -> 1 instead of -1 -> 1
 		run_dot = clamp(run_dot,0,1)
 		move_direction *= run_dot
-	
-	velocity.x = move_direction.x * target_speed
-	velocity.z = move_direction.z * target_speed
+
+	var current_smoothing = acceleration
+	if not is_on_floor:
+		current_smoothing = air_accel
+	elif not move_direction:
+		current_smoothing = braking_speed
+
+	var target_velocity = move_direction * target_speed
+
+	velocity.x = lerp(velocity.x,target_velocity.x,current_smoothing * delta)
+	velocity.z = lerp(move_direction.z,target_velocity.z,current_smoothing * delta)
 
 	# Camera Look
 	self.rotate_y(-camera_look_input.x * look_sensitivty)
@@ -57,6 +66,13 @@ func _physics_process(delta):
 	camera_look_input = Vector2.ZERO
 	# move and sliiiiide
 	move_and_slide()
+
+	# Press escape to toggle mouse visibility (don't keep in production)
+	if Input.is_action_just_pressed("ui_cancel"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
 func _unhandled_input(event):
