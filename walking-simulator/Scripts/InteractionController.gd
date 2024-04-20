ee3b1ddd8d700c88af5811b1c453ac3d90274775
Maarten Broekman <maarten@broekman.us>
2024-04-20 11:31:35 -0400

    mjbroekman - added interactableobject to pedestal

--- walking-simulator/Scripts/InteractionController.gd
+++ walking-simulator/Scripts/InteractionController.gd
@@ -8,4 +8,7 @@ func _process(delta):
 	interact_prompt_label.text = ""
 	
 	if object and object is InteractableObject:
-		pass
+		if object.can_interact:
+			interact_prompt_label.text = "[E]" + object.interact_prompt
+			if Input.is_action_just_pressed("interact"):
+				object._interact()
