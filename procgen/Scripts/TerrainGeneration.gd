176917ec3cd50ead1b89448fe28a020bf0190ca1
Maarten Broekman <maarten@broekman.us>
2024-04-21 16:51:19 -0400

    mjbroekman - procgen world project from Zenva lessons 1 - 9
    Course URL: https://academy.zenva.com/course/godot-procedural-generation/

---
+++ procgen/Scripts/TerrainGeneration.gd
@@ -0,0 +1,86 @@
+class_name TerrainGeneration
+extends Node
+
+@export_category("Mesh Sizing")
+@export var mesh : MeshInstance3D
+@export var size_depth : float = 100
+@export var size_width : float = 100
+@export var mesh_resolution : float = 5
+@export var max_height : float = 50
+
+@export_category("Noise Mapping")
+@export var noise : FastNoiseLite
+@export var elevation_curve : Curve
+
+@export_category("Falloff Settings")
+@export var falloff_image : Image
+@export var use_falloff : bool = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	noise.seed = randi_range(0,Time.get_unix_time_from_datetime_string(Time.get_time_string_from_system()))
+	print(str("Using seed: ", noise.seed))
+	var falloff_texture = preload("res://Procedural Generation/Textures/TerrainFalloff.png")
+	falloff_image = falloff_texture.get_image()
+	generate()
+
+func generate():
+	var plane_mesh = PlaneMesh.new()
+	plane_mesh.size = Vector2(size_width,size_depth)
+	print(plane_mesh.size)
+	plane_mesh.subdivide_depth = size_depth * mesh_resolution
+	plane_mesh.subdivide_width = size_width * mesh_resolution
+	plane_mesh.material = preload("res://Procedural Generation/Materials/TerrainMaterial.tres")
+
+	var surface = SurfaceTool.new()
+	var data = MeshDataTool.new()
+	surface.create_from(plane_mesh, 0) # 0 is the surface ID (first surface is 0)
+
+	var array_plane = surface.commit()
+	data.create_from_surface(array_plane,0)
+
+	for i in range(data.get_vertex_count()):
+		var vertex = data.get_vertex(i)
+		var new_y = get_noise_y(vertex.x,vertex.z)
+		vertex.y = new_y
+		data.set_vertex(i,vertex)
+
+	array_plane.clear_surfaces()
+
+	data.commit_to_surface(array_plane)
+	surface.begin(Mesh.PRIMITIVE_TRIANGLES)
+	surface.create_from(array_plane,0)
+	surface.generate_normals()
+
+	mesh = MeshInstance3D.new()
+	mesh.mesh = surface.commit() # generates the final mesh
+	mesh.create_trimesh_collision() # creates colliders
+	mesh.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
+	mesh.add_to_group("NavSource")
+	add_child(mesh)
+
+# Take x and z coords and look up the location in the noise map
+func get_noise_y(x, z) -> float:
+	var value = noise.get_noise_2d(x,z)  # return a value from -1 to 1
+	var remapped_value = (value + 1) / 2.0 # remap from -1 -> 1 to 0 -> 1
+	var adjusted_value = elevation_curve.sample(remapped_value)
+
+	var x_percent = (x + (size_width / 2.0)) / size_width
+	var z_percent = (z + (size_depth / 2.0)) / size_depth
+
+	var x_pixel = int(x_percent * falloff_image.get_width())
+	var y_pixel = int(z_percent * falloff_image.get_height())
+
+	if x_pixel == falloff_image.get_width():
+		x_pixel -= 1
+
+	if y_pixel == falloff_image.get_height():
+		y_pixel -= 1
+
+	var falloff = falloff_image.get_pixel(x_pixel,y_pixel).r
+
+	if not use_falloff:
+		falloff = 1
+
+	return adjusted_value * max_height * falloff
+
