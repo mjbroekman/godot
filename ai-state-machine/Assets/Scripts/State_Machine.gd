31c41591b74dbd1b15b8facebdb47cbf2979c911
Maarten Broekman <maarten@broekman.us>
2024-06-04 13:28:38 -0400

    mjbroekman - initial wander state; updated StateMachine script

--- ai-state-machine/Assets/Scripts/State_Machine.gd
+++ ai-state-machine/Assets/Scripts/State_Machine.gd
@@ -2,3 +2,51 @@ class_name StateMachine
 extends Node
 
 
+@export var default_state : State
+
+var current_state : State
+var states : Dictionary = {}
+
+func _ready():
+	for child in get_children():
+		if child is State:
+			states[child.name] = child
+			child.initialize()
+
+	if default_state != null:
+		change_state(default_state.name)
+
+
+func change_state(state_name):
+	var new_state = states.get(state_name)
+	
+	if new_state == null:
+		return
+
+	if new_state == current_state:
+		return
+
+	if current_state != null:
+		current_state.exit()
+
+	current_state = new_state
+	new_state.enter()
+
+	print("Change State: " + state_name)
+
+
+func _process(delta):
+	if current_state != null:
+		current_state.update(delta)
+
+
+func _physics_process(delta):
+	if current_state != null:
+		current_state.physics_update(delta)
+
+
+func _on_navigation_agent_3d_target_reached():
+	if current_state != null:
+		current_state.navigation_complete()
+
+
