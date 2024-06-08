da34a4a4c1c97f304c186c520b30d49e5fcc5304
Maarten Broekman <maarten@broekman.us>
2024-06-08 08:33:46 -0400

    mjbroekman - copied the inventory project to expand into the crafting project

---
+++ crafting/Scripts/Item.gd
@@ -0,0 +1,12 @@
+class_name Item
+extends Resource
+
+@export var display_name : String
+@export var icon : Texture2D
+@export var max_stack_size : int = 12
+@export var world_item_scene : PackedScene
+@export var is_consumable = false
+
+func on_use (_player) -> bool:
+	print("Used")
+	return is_consumable
