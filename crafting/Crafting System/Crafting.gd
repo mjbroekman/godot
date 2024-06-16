b1ee935951907af0a16c1c9a6c66dd99278cda68
Maarten Broekman <maarten@broekman.us>
2024-06-16 10:59:00 -0400

    mjbroekman - starting to connect all the pieces and recipes

--- crafting/Crafting System/Crafting.gd
+++ crafting/Crafting System/Crafting.gd
@@ -1,11 +1,22 @@
 extends Node
 
+@onready var window : Panel = get_node("CraftingWindow")
+@onready var ui_parent : VBoxContainer = get_node("CraftingWindow/RecipeContainer")
+
+@export var crafting_recipeui_scene : PackedScene
+@export var recipes : Array[CraftingRecipe]
+
+var recipe_uis : Array[CraftingRecipeUI]
+var inventory : Inventory
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	inventory = get_parent().get_node('Inventory')
+	
+	for recipe in recipes:
+		var recipe_node = crafting_recipeui_scene.instantiate()
+		ui_parent.add_child(recipe_node)
+		recipe_node.recipe = recipe
+		recipe_node.crafting = self
+		recipe_uis.append(recipe_node)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
--- crafting/Crafting System/CraftingRecipeUI.gd
+++ crafting/Crafting System/CraftingRecipeUI.gd
@@ -1,11 +1,19 @@
+class_name CraftingRecipeUI
 extends Node
 
+var item_icon : TextureRect
+var recipe_text : Label
+var craft_button : Button
+var recipe : CraftingRecipe
+var crafting
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func get_nodes():
+	item_icon = get_node('ItemIcon')
+	recipe_text = get_node('RecipeText')
+	craft_button = get_node('CraftButton')
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func update_recipe( inventory : Inventory ):
 	pass
+
+func _on_craft_button_pressed():
+	pass # Replace with function body.
