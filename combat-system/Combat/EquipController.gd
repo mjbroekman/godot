d3a9503bfdfb158b088c1630ccc824ff58a9dc3a
Maarten Broekman <maarten@broekman.us>
2024-06-16 22:29:39 -0400

    mjbroekman - complete attack sequence, added goo, created enemy scene

--- combat-system/Combat/EquipController.gd
+++ combat-system/Combat/EquipController.gd
@@ -1,11 +1,27 @@
 extends Node
 
+@onready var equip_origin : Node3D = get_parent().get_node("Camera3D/EquipOrigin")
+var current_equip : EquipObject # Current item that is the child of equip_origin
+var current_equip_item : EquipItem # item resource representing current_equip
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func equip(item : EquipItem):
+	if current_equip_item == item:
+		unequip()
+		return
+	
+	unequip()
+	
+	var object = item.equip_scene.instantiate()
+	equip_origin.add_child(object)
+	current_equip = object
+	current_equip.player = get_parent()
+	current_equip_item = item
 
+func unequip():
+	if current_equip_item == null:
+		return
+	
+	current_equip.queue_free()
+	current_equip = null
+	current_equip_item = null
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
--- combat-system/Combat/EquipObject.gd
+++ combat-system/Combat/EquipObject.gd
@@ -1,11 +1,19 @@
-extends Node
+class_name EquipObject
+extends Node3D
 
+@onready var animation : AnimationPlayer = get_node("AnimationPlayer")
+var player : PlayerController
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func on_primary_use():
 	pass
+
+func on_secondary_use():
+	pass
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == 1:
+			on_primary_use()
+		elif event.button_index == 2:
+			on_secondary_use()
+
