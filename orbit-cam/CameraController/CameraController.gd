0612b1df5f39ae8732bf53fa0a543be5913688b8
Maarten Broekman <maarten@broekman.us>
2024-06-30 19:10:56 -0400

    mjbroekman - improved controller / objects to allow you to relocate the camera

--- orbit-cam/CameraController/CameraController.gd
+++ orbit-cam/CameraController/CameraController.gd
@@ -1,30 +1,43 @@
+class_name CameraController
 extends CharacterBody3D
 
 @onready var camera : Camera3D = get_node("Camera3D")
 
 @export_category("Camera Motion")
 @export var look_sensitivity : float = 0.0015
+@export var zoom_sensitivity : float = 0.5
 var camera_look_input : Vector2
+var camera_zoom_input : Vector2
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
-	if Input.is_action_just_pressed("ui_cancel"):
-		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
-			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-		else:
-			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-camera_look_input.x * look_sensitivity)
+		rotate_x(-camera_look_input.y * look_sensitivity)
 
-	rotate_y(-camera_look_input.x * look_sensitivity)
-	rotate_x(-camera_look_input.y * look_sensitivity)
-	#camera.rotation.x = clamp(camera.rotation.x, -1.5, 1.5)
 	camera_look_input = Vector2.ZERO
 
 
 func _unhandled_input(event):
-	if event is InputEventMouseMotion:
-		camera_look_input = event.relative
+	if Input.is_action_just_pressed("ui_cancel") or Input.is_key_label_pressed(KEY_ESCAPE) or Input.is_physical_key_pressed(KEY_ESCAPE) or Input.is_key_pressed(KEY_ESCAPE):
+		_toggle_mouse()
+
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			camera_look_input = event.relative
+		elif event.pressed and (event.keycode == 4194305 or event.keycode == "4194305"):
+			_toggle_mouse()
+		else:
+			if event.pressed:
+				print(event.keycode)
+
+
+func _toggle_mouse():
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
--- orbit-cam/TestObject/TestObject.gd
+++ orbit-cam/TestObject/TestObject.gd
@@ -1,11 +1,27 @@
 extends Area3D
 
 @export var last_report_time : float
+@onready var main_scene : Node3D = get_parent()
+@onready var camera = main_scene.find_child("CameraController")
+var camera_obj : PackedScene = load("res://CameraController/OrbitCam.tscn")
 
 func _ready():
 	last_report_time = 0
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+
 func _process(_delta):
 	if Time.get_unix_time_from_system() - last_report_time > 2:
-		print(position)
 		last_report_time = Time.get_unix_time_from_system()
+
+
+func _on_input_event(camera, event, position, normal, shape_idx):
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+		if event is InputEventMouseButton and event.button_index == 1 and event.pressed:
+			camera.get_parent().remove_child(camera)
+			camera.queue_free()
+			var new_cam = camera_obj.instantiate()
+			add_child(new_cam)
+
+
+func _on_mouse_entered():
+	print(str(self) + " => " + str(position))
