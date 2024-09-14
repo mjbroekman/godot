ec53b49d16c82d299cb53827e5099e8bd66f7439
Maarten Broekman <maarten@broekman.us>
2024-09-14 12:15:19 -0400

    mjbroekman - import of assets; basic node setup; basic player setup

---
+++ iga-csharp-maze-game/Scripts/General/GameConstants.cs
@@ -0,0 +1,9 @@
+public class GameConstants
+{
+    // Animation Constants
+    public const string ANIM_IDLE = "Idle";
+    public const string ANIM_HURT = "Hurt";
+    public const string ANIM_MOVE = "Move";
+    public const string ANIM_ATTACK = "Attack";
+    public const string ANIM_DEATH = "Death";
+}
