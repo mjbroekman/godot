6538d5c1a619453fd3515ec6c5ce29cb31da148e
Maarten Broekman <maarten@broekman.us>
2024-09-20 21:02:34 -0400

    mjbroekman - adding state machine scripts

--- iga-csharp-maze-game/Scripts/General/GameConstants.cs
+++ iga-csharp-maze-game/Scripts/General/GameConstants.cs
@@ -5,5 +5,12 @@ public class GameConstants
     public const string ANIM_HURT = "Hurt";
     public const string ANIM_MOVE = "Move";
     public const string ANIM_ATTACK = "Attack";
+    public const string ANIM_ATTACK2 = "Attack2";
+    public const string ANIM_ATTACK3 = "Attack3";
     public const string ANIM_DEATH = "Death";
+
+    // Notifications
+    public const int NOTIFICATION_ENTER_STATE = 5001;
+    public const int NOTIFICATION_EXIT_STATE = 5002;
+
 }
