692367b24db79f339e99f2b729bcaf8b64c2da7a
Maarten Broekman <maarten@broekman.us>
2024-12-17 20:42:29 -0500

    mjbroekman - setup unit/scene reference; allow units to be owned by civs

--- fourx-basicgame-csharp/Scripts/Unit.cs
+++ fourx-basicgame-csharp/Scripts/Unit.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class Unit : Node2D
 {
@@ -7,4 +8,23 @@ public partial class Unit : Node2D
     public string unitName = "DEFAULT";
 
     public int productionRequired = -5;
+
+    public Civilization ownerCiv = null;
+    
+    public static Dictionary<Type, PackedScene> unitSceneResources;
+
+    public static void LoadUnitScenes()
+    {
+        unitSceneResources = new Dictionary<Type, PackedScene> {
+            { typeof(Settler), ResourceLoader.Load<PackedScene>("res://Scenes/Units/Settler.tscn") },
+            { typeof(Warrior), ResourceLoader.Load<PackedScene>("res://Scenes/Units/Warrior.tscn") }
+        };
+    }
+
+    public void SetCiv(Civilization civ)
+    {
+        this.ownerCiv = civ;
+        GetNode<Sprite2D>("UnitSprite").Modulate = civ.territoryColor;
+        this.ownerCiv.units.Add(this);
+    }
 }
