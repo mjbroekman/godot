de8bbe334635ae1f352bddfff2a7b1bc5bc2be6f
Maarten Broekman <maarten@broekman.us>
2025-01-04 17:59:13 -0500

    mjbroekman - capture the mouse on start; add movement controls

--- micro-survival-game/Scripts/Player.gd
+++ micro-survival-game/Scripts/Player.gd
@@ -22,6 +22,8 @@ func _ready():
 	
 	remove_child(camera)
 	get_node("/root/Main").add_child.call_deferred(camera)
+	
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
 func _input(event):
@@ -37,9 +39,28 @@ func _process(delta):
 
 
 func _physics_process(delta):
-	var keypress = Input.get_vector("move_left","move_right","move_forward","move_back")
+	# If we're not on the floor, we're falling... so accelerate
+	if ( ! is_on_floor() ):
+		velocity.y -= gravity * delta
 
-	velocity.x = keypress.x * move_speed
-	velocity.z = keypress.y * move_speed
+	# If we pressed the jump button and we ARE on the ground, add our jump_force
+	if ( Input.is_action_just_pressed("move_jump") and is_on_floor()):
+		velocity.y += jump_force
 	
+	# If we are on the ground, allow us to change our lateral speeds
+	if ( is_on_floor() ):
+		# Get a vector based on the keys being pressed
+		var keypress = Input.get_vector("move_left","move_right","move_forward","move_back")
+
+		# Convert keypress vector from world coordinates to camera coordinates
+		var dir = (camera.basis.z * keypress.y) + (camera.basis.x * keypress.x)
+
+		# Normalize things so we don't slow down when looking up or down
+		dir.y = 0
+		dir = dir.normalized()
+
+		# Apply direction vector to object velocity
+		velocity.x = dir.x * move_speed
+		velocity.z = dir.z * move_speed
+
 	move_and_slide()
