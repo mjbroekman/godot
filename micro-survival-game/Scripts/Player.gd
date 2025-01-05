9216c9d20ff134a2e8036d33dae217579c46aa7b
Maarten Broekman <maarten@broekman.us>
2025-01-04 20:11:25 -0500

    mjbroekman - complete day/night cycle; added trees for extra shadowy fun

--- micro-survival-game/Scripts/Player.gd
+++ micro-survival-game/Scripts/Player.gd
@@ -16,6 +16,8 @@ var head : Node3D
 
 var mouse_dir : Vector2
 
+var game_time : float = 0
+
 func _ready():
 	camera = get_node("Camera3D")
 	head = get_node("Head")
@@ -27,40 +29,46 @@ func _ready():
 
 
 func _input(event):
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion && Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		camera.rotation_degrees.x += event.relative.y * ( -look_sens if invert_mouse else look_sens )
 		camera.rotation_degrees.x = clamp(camera.rotation_degrees.x,min_x_rot,max_x_rot)
 
 		camera.rotation_degrees.y += event.relative.x * ( -look_sens if invert_mouse else look_sens )
 
+	if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE && event is InputEventMouseButton:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _process(delta):
 	camera.position = head.global_position
-
+	game_time += delta
 
 func _physics_process(delta):
-	# If we're not on the floor, we're falling... so accelerate
-	if ( ! is_on_floor() ):
-		velocity.y -= gravity * delta
+	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		# If we're not on the floor, we're falling... so accelerate
+		if ( ! is_on_floor() ):
+			velocity.y -= gravity * delta
 
-	# If we pressed the jump button and we ARE on the ground, add our jump_force
-	if ( Input.is_action_just_pressed("move_jump") and is_on_floor()):
-		velocity.y += jump_force
-	
-	# If we are on the ground, allow us to change our lateral speeds
-	if ( is_on_floor() ):
-		# Get a vector based on the keys being pressed
-		var keypress = Input.get_vector("move_left","move_right","move_forward","move_back")
+		if ( Input.is_action_just_pressed("ui_cancel") ):
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
-		# Convert keypress vector from world coordinates to camera coordinates
-		var dir = (camera.basis.z * keypress.y) + (camera.basis.x * keypress.x)
+		# If we pressed the jump button and we ARE on the ground, add our jump_force
+		if ( Input.is_action_just_pressed("move_jump") and is_on_floor()):
+			velocity.y += jump_force
+		
+		# If we are on the ground, allow us to change our lateral speeds
+		if ( is_on_floor() ):
+			# Get a vector based on the keys being pressed
+			var keypress = Input.get_vector("move_left","move_right","move_forward","move_back")
 
-		# Normalize things so we don't slow down when looking up or down
-		dir.y = 0
-		dir = dir.normalized()
+			# Convert keypress vector from world coordinates to camera coordinates
+			var dir = (camera.basis.z * keypress.y) + (camera.basis.x * keypress.x)
 
-		# Apply direction vector to object velocity
-		velocity.x = dir.x * move_speed
-		velocity.z = dir.z * move_speed
+			# Normalize things so we don't slow down when looking up or down
+			dir.y = 0
+			dir = dir.normalized()
 
-	move_and_slide()
+			# Apply direction vector to object velocity
+			velocity.x = dir.x * move_speed
+			velocity.z = dir.z * move_speed
+
+		move_and_slide()
