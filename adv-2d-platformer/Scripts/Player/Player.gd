5e663ed086272343fe5f5810e2474813ca377da2
Maarten Broekman <maarten@broekman.us>
2025-01-23 17:37:12 -0500

    mjbroekman - add statemachine scripts; update player to be more state-machine friendly

--- adv-2d-platformer/Scripts/Player/Player.gd
+++ adv-2d-platformer/Scripts/Player/Player.gd
@@ -1,34 +1,68 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+###
+# Animations
+###
+@onready var anim : AnimatedSprite2D = get_node("AnimatedSprite2D")
 
-var has_double_jump : bool = false
+###
+# Movement variables
+###
+# const SPEED = 300.0
+# const JUMP_VELOCITY = -400.0
+
+@export var max_speed : int = 8000
+@export var acceleration : int = 1000
+@export var jump_height : int = 15000
+var direction : int = 0
+
+@export var friction : float = 0.22
+@export var weight : float = 0.35
+
+###
+# State Info
+###
+var current_state : String = ""
 var is_jumping : bool = false
+@export var can_dash : bool = true
+@export var max_jumps : int = 3
+@export var jump_count : int = 0
+@export var has_double_jump : bool = false
 
 func _physics_process(delta):
-	# Add the gravity.
+	# Account for gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		# Alternate gravity calculation
+		var target_velocity : float = min( (velocity.y + acceleration * delta), (max_speed * delta) )
+		velocity.y = lerp(velocity.y, target_velocity, 0.6)
+		# Original gravity calculation
+		#velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		is_jumping = true
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+		#is_jumping = true
 
-	if Input.is_action_just_pressed("ui_accept") and is_jumping and has_double_jump and not is_on_floor():
-		velocity.y += JUMP_VELOCITY
+	#if Input.is_action_just_pressed("ui_accept") and is_jumping and has_double_jump and not is_on_floor():
+		#velocity.y += JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	if is_on_floor():
-		is_jumping = false
+	# Flip the animated sprite
+	if direction < 0:
+		anim.flip_h = false
+	elif direction > 0:
+		anim.flip_h = true
+
+	#if direction:
+		#velocity.x = direction * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	#if is_on_floor():
+		#is_jumping = false
 
 	move_and_slide()
