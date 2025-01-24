1441df6ae801553b7a8dfcd20d985406f14ef22d
Maarten Broekman <maarten@broekman.us>
2025-01-24 18:20:04 -0500

    mjbroekman - renamed MoveState to WalkState

---
+++ adv-2d-platformer/Scripts/Player/States/WalkState.gd
@@ -0,0 +1,16 @@
+extends Node
+class_name WalkState
+
+@onready var player: Node = get_parent().get_parent()
+
+func reset_state() -> void:
+	exit_state()
+	start_state()
+
+func start_state() -> void:
+	player.anim.play("Walk",1.0)
+	player.jump_count = 0
+	player.can_dash = true
+
+func exit_state() -> void:
+	player.anim.stop()
