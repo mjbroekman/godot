eb001c38026fccb206697fe818db7dc0734bada4
Maarten Broekman <maarten@broekman.us>
2025-02-04 18:47:09 -0500

    mjbroekman - lesson 27
    - renamed ground-based enemy cliff raycast to ground
    
    - added flying mob
    
    - added nodes for organization purposes

--- adv-2d-platformer/Scripts/Monsters/FlyingMob.gd
+++ adv-2d-platformer/Scripts/Monsters/FlyingMob.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 const SPEED = 30.0
 
 var change_dir : bool = false
-@onready var ray_cliff : RayCast2D = get_node("GroundCheck")
+@onready var ray_ground : RayCast2D = get_node("GroundCheck")
 @onready var ray_wall : RayCast2D = get_node("WallCheck")
 var move_dir : int = -1
 var am_dead : bool = false
@@ -16,11 +16,8 @@ func _physics_process(delta):
 		self.scale.y = new_scale
 		return
 
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	if ( ! ray_cliff.is_colliding() ) || ray_wall.is_colliding():
+#	if ( ! ray_ground.is_colliding() ) || ray_wall.is_colliding():
+	if ( ray_ground.is_colliding() ):
 		print("Switch direction!")
 		change_dir = !change_dir
 		if change_dir:
@@ -28,15 +25,16 @@ func _physics_process(delta):
 		else:
 			move_dir = -1
 
-	ray_cliff.position.x = abs(ray_cliff.position.x) * move_dir
+	ray_ground.target_position.y = abs(ray_ground.target_position.y) * move_dir
 	ray_wall.target_position.x = abs(ray_wall.target_position.x) * move_dir
 
-	print("Cliff RayCast2D X position = " + str(ray_cliff.position.x))
+	print("Ground RayCast2D Y target = " + str(ray_ground.target_position.y))
 	print("Wall RayCast2D X target = " + str(ray_wall.target_position.x))
 	print("Change direction? " + str(change_dir))
 	print("Move direction? " + str(move_dir))
 
-	velocity.x = SPEED * move_dir
+	velocity.y = SPEED * move_dir
+	velocity.x = 0.0
 	get_node('AnimatedSprite2D').flip_h = change_dir
 
 	move_and_slide()
