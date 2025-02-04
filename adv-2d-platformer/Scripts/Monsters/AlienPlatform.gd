b93bb6293737ff8d5681dde41b9bfd022c4a9b69
Maarten Broekman <maarten@broekman.us>
2025-02-03 21:11:36 -0500

    mjbroekman - lesson 25 and 26
    - add damage colliders for enemy
    
    - allow player to dash through enemy to kill it
    
    - fix player collider

--- adv-2d-platformer/Scripts/Monsters/AlienPlatform.gd
+++ adv-2d-platformer/Scripts/Monsters/AlienPlatform.gd
@@ -7,8 +7,15 @@ var change_dir : bool = false
 @onready var ray_cliff : RayCast2D = get_node("CliffCheck")
 @onready var ray_wall : RayCast2D = get_node("WallCheck")
 var move_dir : int = -1
+var am_dead : bool = false
 
 func _physics_process(delta):
+	if am_dead:
+		var new_scale = 1.0 - get_node("DeathAudio").get_playback_position()
+		self.scale.x = new_scale
+		self.scale.y = new_scale
+		return
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -33,3 +40,29 @@ func _physics_process(delta):
 	get_node('AnimatedSprite2D').flip_h = change_dir
 
 	move_and_slide()
+
+
+func _on_head_damage_body_entered(body):
+	if "Player" in body.name:
+		death()
+
+func _on_player_damage_body_entered(body):
+	if "Player" in body.name:
+		if "Dash" in body.current_state:
+			death()
+		else:
+			Game.health -= 1
+			if body.get_node("Hit") != null:
+				body.get_node("Hit").play()
+
+func death():
+	am_dead = true
+	get_node("BodyCollider").queue_free()
+	get_node("PlayerDamage").queue_free()
+	get_node("CliffCheck").queue_free()
+	get_node("WallCheck").queue_free()
+	get_node("DeathAudio").play()
+
+
+func _on_death_audio_finished():
+	queue_free()
