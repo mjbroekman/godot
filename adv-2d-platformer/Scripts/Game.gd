0bdfcfb718a5b21649225b5ecb12dadce45c357b
Maarten Broekman <maarten@broekman.us>
2025-02-06 18:10:11 -0500

    mjbroekman - lesson 29
    - attached gameover scene and script to the rest of the game
    
    - added a reset_game function to the global script

--- adv-2d-platformer/Scripts/Game.gd
+++ adv-2d-platformer/Scripts/Game.gd
@@ -1,5 +1,11 @@
 extends Node
 
 @export var health : int = 4
+@export var starting_health : int = 4
 @export var max_health : int = 4
 @export var gold : int = 0
+
+func reset_game():
+	health = starting_health
+	max_health = starting_health
+	gold = 0
