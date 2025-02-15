66c3af6034a2c06a39e5c92fada640af7f45b2a0
Maarten Broekman <maarten@broekman.us>
2025-02-15 10:52:34 -0500

    mjbroekman - start adding physics and movement

--- local-multiplayer-fighter/Scripts/Player.gd
+++ local-multiplayer-fighter/Scripts/Player.gd
@@ -1,25 +1,25 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+var move_speed : float = 600.0
+var jump_force : float = 1000.0
+var gravity : float = 2800.0
+var char_dir : float = 0.0
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump_p1") and is_on_floor():
+		velocity.y -= jump_force
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	char_dir = Input.get_axis("move_left_p1", "move_right_p1")
+	if char_dir:
+		velocity.x = char_dir * move_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, move_speed)
 
 	move_and_slide()
