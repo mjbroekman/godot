74775086a6b0dd8f3eeea576019d22c443b1c730
Maarten Broekman <maarten@broekman.us>
2025-02-16 16:29:29 -0500

    mjbroekman - set up control dictionary for p1 and p2

--- local-multiplayer-fighter/Scripts/Global.gd
+++ local-multiplayer-fighter/Scripts/Global.gd
@@ -1,3 +1,20 @@
 extends Node
 
 @export var game_mode : String = ""
+
+var keybindings : Dictionary = {
+	"Player1" : {
+		"up" : "move_up_p1",
+		"left" : "move_left_p1",
+		"right" : "move_right_p1",
+		"jump" : "jump_p1",
+		"fire" : "fire_p1"
+	},
+	"Player2": {
+		"up" : "move_up_p2",
+		"left" : "move_left_p2",
+		"right" : "move_right_p2",
+		"jump" : "jump_p2",
+		"fire" : "fire_p2"
+	}
+}
