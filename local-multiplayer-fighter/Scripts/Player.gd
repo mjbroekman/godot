92d04ecf7e7de72fbf76ac59dcef74101a778ecf
Maarten Broekman <maarten@broekman.us>
2025-02-16 16:29:58 -0500

    mjbroekman - use the node name to determine if we are P1 or P2 and check corresponding controls

--- local-multiplayer-fighter/Scripts/Player.gd
+++ local-multiplayer-fighter/Scripts/Player.gd
@@ -1,9 +1,27 @@
 extends CharacterBody2D
 
-var move_speed : float = 600.0
-var jump_force : float = 1000.0
-var gravity : float = 2800.0
+@export var move_speed : float = 600.0
+@export var jump_force : float = 1000.0
+@export var gravity : float = 2800.0
 var char_dir : float = 0.0
+var player : String = ""
+var controls : Dictionary
+
+func _ready():
+	if Global.keybindings.has(name):
+		print("Using keybindings for " + name)
+		player = name
+	
+	if name == "Player":
+		print("Defaulting to controls for player 1")
+		player = "Player1"
+	
+	if name == "":
+		print("Error... Player node name not discoverable. Exiting...")
+		get_tree().quit()
+
+	controls = Global.keybindings[player]
+
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -11,12 +29,17 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump_p1") and is_on_floor():
+	if Input.is_action_just_pressed(controls["jump"]) and is_on_floor():
 		velocity.y -= jump_force
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	char_dir = Input.get_axis("move_left_p1", "move_right_p1")
+	char_dir = Input.get_axis(controls["left"], controls["right"])
+	if char_dir > 0:
+		get_node("Sprite2D").flip_h = false
+	if char_dir < 0:
+		get_node("Sprite2D").flip_h = true
+
 	if char_dir:
 		velocity.x = char_dir * move_speed
 	else:
