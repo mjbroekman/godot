fa9ef5206d81cca2f5079152a14cf46746c699ac
Maarten Broekman <maarten@broekman.us>
2025-02-18 17:56:08 -0500

    mjbroekman - converted player to animated sprite and added animations

--- local-multiplayer-fighter/Scripts/Player.gd
+++ local-multiplayer-fighter/Scripts/Player.gd
@@ -27,6 +27,7 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		get_node("PlayerSprite").play("Jump")
 
 	# Handle jump.
 	if Input.is_action_just_pressed(controls["jump"]) and is_on_floor():
@@ -36,13 +37,15 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	char_dir = Input.get_axis(controls["left"], controls["right"])
 	if char_dir > 0:
-		get_node("Sprite2D").flip_h = false
+		get_node("PlayerSprite").flip_h = false
 	if char_dir < 0:
-		get_node("Sprite2D").flip_h = true
+		get_node("PlayerSprite").flip_h = true
 
 	if char_dir:
 		velocity.x = char_dir * move_speed
+		get_node("PlayerSprite").play("Walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, move_speed)
+		get_node("PlayerSprite").play("Idle")
 
 	move_and_slide()
